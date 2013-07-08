75669804bf98721f51cfc80e2f28ee8f7a54731b
Dan Carley <dan.carley@gmail.com>
2013-07-08 18:17:41 +0100

    Add updatemotd::fragment defined type
    A defined type for installing new fragments in `/etc/update-motd.d`.
    Documented and tested.

---
+++ manifests/fragment.pp
@@ -0,0 +1,47 @@
+# == Define: updatemotd::fragment
+#
+# Define a fragment to be included by motd(5) / update-motd(5). The contents
+# should be executable and write to STDOUT.
+#
+# === Parameters
+#
+# [*order*]
+#   Two digit integer between 00 and 99. This is the order which the file
+#   will be included in the motd output.
+#   Default: 50
+#
+# [*source*]
+#   Standard `source` param for a file resource. This is mutually exclusive
+#   to the `content` param.
+#   Default: undef
+#
+# [*content*]
+#   Standard `content` param for a file resource. This is mutually exclusive
+#   to the `source` param.
+#   Default: undef
+#
+#
+define updatemotd::fragment(
+  $order = 50,
+  $source = undef,
+  $content = undef
+) {
+  validate_re($title, '^[a-zA-Z_]+$')
+  validate_re($order, '^[0-9]{2}$')
+
+  if !($source) and !($content) {
+    fail('source or content params are required')
+  } elsif ($source) and ($content) {
+    fail('source and content params are mutually exclusive')
+  }
+
+  include updatemotd::params
+
+  file { "${updatemotd::params::config_dir}/${order}-${title}":
+    ensure  => present,
+    mode    => '0755',
+    source  => $source,
+    content => $content,
+    require => Class['updatemotd'],
+  }
+}
---
+++ spec/defines/fragment_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+
+describe 'updatemotd::fragment' do
+  let(:facts) {{
+    :osfamily => 'Debian',
+  }}
+
+  let(:title) { 'giraffe' }
+  let(:config_dir) { '/etc/update-motd.d' }
+  let(:config_file) { "#{config_dir}/50-giraffe" }
+
+  describe 'order' do
+    context 'valid values' do
+      [30, '40'].each do |order, title|
+        describe title.inspect do
+          let(:params) {{
+            :order   => order,
+            :content => 'lion',
+          }}
+
+          it { should contain_file("#{config_dir}/#{order}-giraffe") }
+        end
+      end
+    end
+
+    context 'invalid values' do
+      [1, 'ten', '100'].each do |order, title|
+        describe title.inspect do
+          let(:params) {{
+            :order   => order,
+            :content => 'lion',
+          }}
+
+          it { expect { should }.to raise_error(Puppet::Error,
+            /does not match/
+          )}
+        end
+      end
+    end
+  end
+
+  describe 'source' do
+    let(:params) {{
+      :source => 'puppet:///zoo/zebra',
+    }}
+
+    it { should contain_file(config_file).with(
+      :mode    => '0755',
+      :source  => 'puppet:///zoo/zebra',
+      :content => nil,
+      :require => 'Class[Updatemotd]'
+    )}
+  end
+
+  describe 'content' do
+    let(:params) {{
+      :content => 'ZEBRA!',
+    }}
+
+    it { should contain_file(config_file).with(
+      :mode    => '0755',
+      :source  => nil,
+      :content => 'ZEBRA!',
+      :require => 'Class[Updatemotd]'
+    )}
+  end
+
+  describe 'no source or content' do
+    let(:params) {{ }}
+
+    it { expect { should }.to raise_error(Puppet::Error,
+      /source or content params are required/
+    )}
+  end
+
+  describe 'source and content' do
+    let(:params) {{
+      :content => 'oh no',
+      :source  => 'puppet:///you/didnt',
+    }}
+
+    it { expect { should }.to raise_error(Puppet::Error,
+      /source and content params are mutually exclusive/
+    )}
+  end
+end
